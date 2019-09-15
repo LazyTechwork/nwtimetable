c7b4d3c4287b532feec8d02f751093307eeb7866
LazyTechwork <lazytechwork@gmail.com>
2019-09-15 22:09:51 +0300

    Updates

---
+++ precache-manifest.b4d9e8eec04525f76ef23b8b9d995891.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f28f85f232e146c58cd8",
+    "url": "/nwtimetable/static/css/main.ad7ae6d5.chunk.css"
+  },
+  {
+    "revision": "f28f85f232e146c58cd8",
+    "url": "/nwtimetable/static/js/main.cb62ce89.chunk.js"
+  },
+  {
+    "revision": "bf05f999a83cf84f0f7f",
+    "url": "/nwtimetable/static/js/runtime~main.bd076a60.js"
+  },
+  {
+    "revision": "6ed676314bbe9443701c",
+    "url": "/nwtimetable/static/css/2.9686c3aa.chunk.css"
+  },
+  {
+    "revision": "6ed676314bbe9443701c",
+    "url": "/nwtimetable/static/js/2.5f9b98f8.chunk.js"
+  },
+  {
+    "revision": "6265858a15f77662415fdb8b71f42fbe",
+    "url": "/nwtimetable/static/media/nwtt_full.6265858a.svg"
+  },
+  {
+    "revision": "12b972f59d3ee45d9ff2fe1346a36fff",
+    "url": "/nwtimetable/index.html"
+  }
+];
