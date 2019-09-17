85b139eeeaed4c2dde285d451c32ebe4c25b842b
LazyTechwork <lazytechwork@gmail.com>
2019-09-17 15:52:04 +0300

    Updates

--- precache-manifest.090d6f9e0fbd357fe798ba8c3c936be0.js
+++ precache-manifest.090d6f9e0fbd357fe798ba8c3c936be0.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = [
   {
-    "revision": "b4fb3de9f8c34549dc57",
+    "revision": "853f85835bb5b2195b70",
     "url": "/nwtimetable/static/css/main.981e17a2.chunk.css"
   },
   {
-    "revision": "b4fb3de9f8c34549dc57",
-    "url": "/nwtimetable/static/js/main.cb62ce89.chunk.js"
+    "revision": "853f85835bb5b2195b70",
+    "url": "/nwtimetable/static/js/main.7dce35f7.chunk.js"
   },
   {
     "revision": "bf05f999a83cf84f0f7f",
     "url": "/nwtimetable/static/js/runtime~main.bd076a60.js"
   },
   {
-    "revision": "6ed676314bbe9443701c",
+    "revision": "39e052e8bb17ae526701",
     "url": "/nwtimetable/static/css/2.9686c3aa.chunk.css"
   },
   {
-    "revision": "6ed676314bbe9443701c",
-    "url": "/nwtimetable/static/js/2.5f9b98f8.chunk.js"
+    "revision": "39e052e8bb17ae526701",
+    "url": "/nwtimetable/static/js/2.247e1a35.chunk.js"
   },
   {
     "revision": "6265858a15f77662415fdb8b71f42fbe",
     "url": "/nwtimetable/static/media/nwtt_full.6265858a.svg"
   },
   {
-    "revision": "f73f3ee398c17be54833681349f769c5",
+    "revision": "d5add00cb9146a2945996dde0b099ed2",
     "url": "/nwtimetable/index.html"
   }
 ];
