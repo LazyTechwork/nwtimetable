226ef467b91f9ad2e3263ba49ac54802e2cbb47b
LazyTechwork <lazytechwork@gmail.com>
2019-09-17 16:09:49 +0300

    Some good styles for Home page

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {FormLayout, Panel, PanelHeader, SelectMimicry} from '@vkontakte/vkui';
+import {FormLayout, Panel, PanelHeader, SelectMimicry, Button, Div} from '@vkontakte/vkui';
 import logo_full from '../img/nwtt_full.svg';
 import '../main.css';
 
@@ -7,13 +7,16 @@ const Home = props => (
     <Panel id={props.id} theme={props.state.theme}>
         <PanelHeader>NeoWave Timetable</PanelHeader>
         <img src={logo_full} alt="Logo" className="logo"/>
-
         <FormLayout>
             <SelectMimicry
                 top="Выберите свой класс"
                 placeholder="Не выбран"
                 onClick={() => props.actions.go('ChooseGroup')}
             >{props.state.group}</SelectMimicry>
+
+                <Div style={{display: 'flex'}}>
+                    <Button level={"primary"} size={"l"} stretched disabled={props.state.group === ''}>Продолжить</Button>
+                </Div>
         </FormLayout>
     </Panel>
 );
